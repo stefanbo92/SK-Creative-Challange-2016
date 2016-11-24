33feb73f520e7c8c27acf1fb45fedb35a8915073
stefan <stefan.boschenriedter@gmx.de>
2016-11-24 14:34:30 +0900

    sounds added

--- Raspi/sounds/sound.py
+++ Raspi/sounds/sound.py
@@ -5,6 +5,13 @@ import time
 pygame.init()
 
 pygame.mixer.music.load("1.mp3")
-
 pygame.mixer.music.play()
+time.sleep(3)
+pygame.mixer.music.load("2.mp3")
+pygame.mixer.music.play()
+time.sleep(3)
+pygame.mixer.music.load("4.mp3")
+pygame.mixer.music.play()
+
+
 print "done"
