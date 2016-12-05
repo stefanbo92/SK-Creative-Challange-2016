437d57ecaf55b8ac88183cf6b98a1e6a4f302cc5
stefan <stefan.boschenriedter@gmx.de>
2016-12-05 16:28:13 +0900

    camera test for adjustment

---
+++ camTest.py
@@ -0,0 +1,19 @@
+import cv2
+cap=cv2.VideoCapture(0)
+
+while (True):
+    ret,img=cap.read()
+    #print img.shape
+    #crop image [y1:y2,x1:x2]
+    img=img[1:400, 200:500]
+    #rotate image
+    rows,cols,ch =img.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+    img = cv2.warpAffine(img,M,(cols,rows))
+    cv2.imshow("img",img)
+    pressed=cv2.waitKey(1)
+    if pressed==107: #if 'k' is pressed
+        break
+
+cap.release()
+cv2.destroyAllWindows()
